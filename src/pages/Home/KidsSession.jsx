3415cb4be40934ab3bfbcd8a8f60f5d498b5e50c
Diego Terto Martins <diegotertomartins@gmail.com>
2024-11-04 10:24:35 -0500

    Home page for desktop

--- src/pages/Home/KidsSession.jsx
+++ src/pages/Home/KidsSession.jsx
@@ -6,13 +6,15 @@ export default function KidsSession() {
     <section className={styles['kids-session']}>
       <div className={styles['kids-session-container']}>
         <h2>Empower Kids with Our BJJ Kids Program</h2>
-        <img src={kidsImg} alt="Kids Class" />
-        <p>
-          Our Kids Program is focused on empowering young champions. Through
-          carefully designed classes, kids learn discipline, respect, and
-          self-confidence while having fun. Our experienced instructors ensure a
-          safe and supportive environment for all participants.
-        </p>
+        <div className={styles['content-container']}>
+          <img src={kidsImg} alt="Kids Class" />
+          <p>
+            Our Kids Program is focused on empowering young champions. Through
+            carefully designed classes, kids learn discipline, respect, and
+            self-confidence while having fun. Our experienced instructors ensure
+            a safe and supportive environment for all participants.
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/pages/Home/Location.jsx
+++ src/pages/Home/Location.jsx
@@ -6,14 +6,18 @@ export default function Location() {
     <section className={styles['location']}>
       <div className={styles['location-container']}>
         <h2>Enhanced BJJ Location</h2>
-        {/* TODO: Add link when do the Google Business profile */}
-        <a href="https://maps.app.goo.gl/x87H2bLNakcW4cJm7" target="_blank">
-          303 Richmond Street, London, Ontario, N6B 2H8
-        </a>
-        <p>
-          <span>*</span>Use the Bathurst Street Entrance
-        </p>
-        <Map latitude={42.9799} longitude={-81.2482} />
+        <div className={styles['content']}>
+          {/* TODO: Add link when do the Google Business profile */}
+          <div className={styles['address']}>
+            <a href="https://maps.app.goo.gl/x87H2bLNakcW4cJm7" target="_blank">
+              303 Richmond Street, London, Ontario, N6B 2H8
+            </a>
+            <p className={styles['expeption']}>
+              <span>*</span> Use the Bathurst Street Entrance
+            </p>
+          </div>
+          <Map latitude={42.9799} longitude={-81.2482} />
+        </div>
       </div>
     </section>
   );
